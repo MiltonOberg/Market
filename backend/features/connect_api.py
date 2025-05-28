995da9887bad52a384f248f5c56c7c42c4631c8d
Milton Österberg <miltonwendel@outlook.com>
2025-05-28 13:53:03 +0200

    lite framsteg på agent

--- backend/features/connect_api.py
+++ backend/features/connect_api.py
@@ -1,15 +1,10 @@
-import requests
-from dotenv import load_dotenv
-import os
+import yfinance as yf
+
 
 class Market:
-    load_dotenv()
-    api_key = os.getenv("API_KEY")
     def __init__(self):
-        self.data = self.get_data()
-    
-    def get_data(self):
-        url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={self.api_key}'
-        r = requests.get(url)
-        data = r.json()
-        return data
+        self.df = None
+
+    def get_data(self, stonk):
+        data = yf.Ticker(f"{stonk}")
+        self.df = data.history(period="2y", interval="1d")
