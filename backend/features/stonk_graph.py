38dc6da0c5237cea1fc8c86ba817a32327997c7a
Milton Österberg <miltonwendel@outlook.com>
2025-06-04 15:36:27 +0200

    bort med agent

--- backend/features/stonk_graph.py
+++ backend/features/stonk_graph.py
@@ -1,10 +1,12 @@
-from backend.features.stonk import Stonk
 import plotly.express as px
 
+from backend.features.stonk import Stonk
+
+
 class StonkGraph:
-    def __init__(self, stonk:str):
+    def __init__(self, stonk: str):
         self.df = Stonk(stonk).df
-        
+
     def date_close(self):
-        fig = px.line(self.df, x= self.df.index, y="Close", title="Stonk Closing Prices")
+        fig = px.line(self.df, x=self.df.index, y="Close", title="Stonk Closing Prices")
         return fig
