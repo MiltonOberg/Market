743982ed7391620b7779049913b293d91100837a
Milton Österberg <miltonwendel@outlook.com>
2025-06-04 15:33:58 +0200

    requirements, small fixes

--- backend/features/stonk.py
+++ backend/features/stonk.py
@@ -7,10 +7,16 @@ class Stonk:
         self.df = self._get_data(stonk)
 
     def _get_symbol(self, stonk: str):
-        result = search(stonk)
-        return result["quotes"][1]["symbol"]
+        try:
+            result = search(stonk)
+            return result["quotes"][1]["symbol"]
+        except Exception as e:
+            raise ValueError(f"Error fetching data for {stonk}: {e}")
 
     def _get_data(self, stonk: str, period: str = "2y", interval: str = "1d"):
         symbol = self._get_symbol(stonk)
-        data = yf.Ticker(symbol)
-        return data.history(period=period, interval=interval)[::-1]
+        try:
+            data = yf.Ticker(symbol)
+            return data.history(period=period, interval=interval)[::-1]
+        except Exception as e:
+            raise ValueError(f"Error fetching data for {stonk}: {e}")
--- frontend/ui/choose_stonk.py
+++ frontend/ui/choose_stonk.py
@@ -5,17 +5,20 @@ from backend.features.stonk_graph import StonkGraph
 
 class ChooseStonk:
     def show_data(self):
-        choice = st.text_input("What stonk do you want to analyse?")
-        stonk = Stonk(choice)
-        st.dataframe(stonk.df)
 
-        stonk_graph = StonkGraph(choice)
+        choice = st.text_input(  "What stonk do you want to analyse?")
+        if choice:
+            stonk = Stonk(choice)
+            stonk_graph = StonkGraph(choice)
 
-        if "show_graph" not in st.session_state:
-            st.session_state.show_graph = False
+            if stonk:
+                st.dataframe(stonk.df)
 
-        if st.button("Graph"):
-            st.session_state.show_graph = not st.session_state.show_graph
-            
-        if st.session_state.show_graph:
-            st.plotly_chart(stonk_graph.date_close())
+            if "show_graph" not in st.session_state:
+                st.session_state.show_graph = False
+
+            if st.button("Graph"):
+                st.session_state.show_graph = not st.session_state.show_graph
+
+            if st.session_state.show_graph:
+                st.plotly_chart(stonk_graph.date_close())
