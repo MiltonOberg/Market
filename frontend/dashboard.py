af3f7f7ab09e5f0375198e30e7d136680d995594
Milton Österberg <miltonwendel@outlook.com>
2025-06-04 13:58:53 +0200

    missad __ini__.py

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -5,7 +5,6 @@ from frontend.choose_stonk import ChooseStonk
 def main():
     st.title("Hello World!")
     st.sidebar.title("Navigation")
-    ChooseStonk().show_data()
 
 
 if __name__ == "__main__":
