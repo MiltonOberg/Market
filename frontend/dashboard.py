9760d79dea8037a20470be50ee6bee36937c1a01
Milton <miltonwendel@outlook.com>
2025-10-22 20:57:08 +0200

    colors

--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -15,29 +15,33 @@ def main():
         st.title("Hello World!")
 
     if page == "Stock Analysis":
-        st.title("Stock Analysis")
-        time = st.radio(
-            "What time frame would you like to see?",
-            [option for option in PERIOD_MAP.keys()],
-            horizontal=True,
-        )
-        choice = st.text_input("What stonk do you want to analyse?", key="analysis")
+        with st.container(key="stock_analysis"):
+            st.header("Stock Analysis")
+            time = st.radio(
+                "What time frame would you like to see?",
+                [option for option in PERIOD_MAP.keys()],
+                horizontal=True,
+            )
+            choice = st.text_input("What stonk do you want to analyse?", key="analysis")
 
-        if choice and time:
-            stonk_analysis = StockAnalysis(choice=choice, timeframe=PERIOD_MAP[time])
-            stonk_analysis.show_table()
-            stonk_analysis.show_graph()
+            if choice and time:
+                stonk_analysis = StockAnalysis(
+                    choice=choice, timeframe=PERIOD_MAP[time]
+                )
+                stonk_analysis.show_table()
+                stonk_analysis.show_graph()
 
     if page == "AI Model":
-        st.title("AI Model")
-        days = st.slider("Days to predict", min_value=0, max_value=30)
-        choice = st.text_input("What stonk do you want to analyse?", key="ai")
+        with st.container(key="ai_model"):
+            st.header("AI Model")
+            days = st.slider("Days to predict", min_value=0, max_value=30)
+            choice = st.text_input("What stonk do you want to analyse?", key="ai")
 
-        if choice:
-            preds = PredictStock(choice=choice).predict_days(days=days)
-            stock_analysis = StockAnalysis(choice=preds)
-            stock_analysis.show_table()
-            stock_analysis.show_graph()
+            if choice:
+                preds = PredictStock(choice=choice).predict_days(days=days)
+                stock_analysis = StockAnalysis(choice=preds)
+                stock_analysis.show_table()
+                stock_analysis.show_graph()
 
 
 if __name__ == "__main__":
